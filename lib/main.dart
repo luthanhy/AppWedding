6f5636b7df0e63a0136bfbad52ff29f499c21bc8
CHUNKINNN\lt230 <lt2302k@gmail.com>
2024-12-29 21:01:43 +0700

    ad d config main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:appwedding/pages/WeddingPage.dart';
 import 'package:flutter/material.dart';
+import 'package:appwedding/pages/ScreenMain.dart';
 
 void main() {
-  runApp(WeddingApp());
+  int config = 1;
+  if (config == 1) {
+    runApp(MyApp());
+  } else {
+    runApp(WeddingApp());
+  }
 }
