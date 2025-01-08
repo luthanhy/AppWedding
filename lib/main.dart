0dd47230adebeb2bff0522fb26337109c291937d
luthanhy <luthanhy1@gmail.com>
2025-01-08 13:41:36 +0700

    done ftue

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
+import './screen/FTUE/index.dart'; // Đảm bảo bạn import màn hình FTUE đúng
 
-void main() {}
+// Đây là widget chính của ứng dụng của bạn.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+          IntroductionAnimationScreen(), // Gọi màn hình FTUE thay vì Scaffold mặc định
+    );
+  }
+}
+
+// Hàm main khởi tạo ứng dụng của bạn
+void main() {
+  runApp(MyApp()); // Gọi ứng dụng MyApp
+}
