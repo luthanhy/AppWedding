d505fcf90a3d3f1dff35ebe9d6d3c47d35d1f823
luthanhy <luthanhy1@gmail.com>
2025-01-08 13:09:19 +0700

    new  app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,3 @@
-import 'package:appwedding/pages/WeddingPage.dart';
 import 'package:flutter/material.dart';
-import 'package:appwedding/pages/ScreenMain.dart';
 
-void main() {
-  int config = 1;
-  if (config == 1) {
-    runApp(MyApp());
-  } else {
-    runApp(Weddingpage());
-  }
-}
+void main() {}
